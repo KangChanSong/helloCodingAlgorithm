b3359701f3f9f475e272b1b5d39e86944b6e5bc4
KangChanSong <thdrkdcks@naver.com>
2021-07-26 16:52:57 +0900

    이진_탐색

---
+++ binarySearch/binarysearch.py
@@ -0,0 +1,20 @@
+
+def binary_search(list, item):
+    low = 0;
+    high = len(list)-1;
+
+    while low <= high:
+        mid = (low+high) // 2
+        guess = list[mid]
+        if item == guess:
+            return mid
+        if item < guess:
+            high = mid - 1;
+        else:
+            low = mid + 1;
+    return None;
+
+my_list = [1,3,5,7,9];
+
+if __name__ == '__main__':
+    print(binary_search(my_list, 5));
